23d6a7ec0cd76f836b876b45cdb2947ebcc83fe4
tmoraru <tatianamoraru1998@gmail.com>
2020-09-09 17:29:18 +0000

    Arguments to the bash Scripting

---
+++ Introduction/arguments.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# use predefined variables to acces passed arguments
+# echo arguments to the shell
+
+echo $1 $2 $3 '-> echo $1 $2 $3'
+
+# We can also store arguments from bash command line in special array
+arg=("$@")
+
+# echo arguments to the shell
+echo ${args[0]} ${args[2]} '-> args=("$@"); echo ${arrgs[0]} ${args[2]}'
+
+# use $@ to print  out all arguments at once
+echo $@ '-> echo $@'
+
+# use $# variable to print out
+# number of arguments passed to the bash script
+echo Number of arguments passed: $# '-> echo Number of arguments passed: $#'
+
