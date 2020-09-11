ba7043ed32e315203e219dd23714a75eb6092203
tmoraru <tatianamoraru1998@gmail.com>
2020-09-11 20:31:45 +0000

    install pkg and start one pkg

---
+++ Scripts/task5.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+ 
+#Install listed packages and start one specific package
+
+packages="httpd curl vim "
+
+for items in $packages;
+do 
+  echo "intalling $items" 
+  sleep 2
+  sudo  yum install $items 
+  sleep 3
+  if [ "$items" == "httpd" ]; then
+   sudo systemctl start $items
+   sudo systemctl status $items
+  fi
+ done
+
