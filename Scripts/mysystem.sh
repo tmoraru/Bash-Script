2dd61ce10afd1fb915c47c92f1d6ba1cdbf930ba
Tatiana Moraru <tatianamoraru1998@gmail.com>
2021-03-17 21:26:20 -0500

    Update mysystem.sh

--- Scripts/mysystem.sh
+++ Scripts/mysystem.sh
@@ -7,7 +7,7 @@ echo -e "Today's date is `date`, this week `date +"%V" `. \n"
 echo -e "These users are currently connected \n"
 w | cut -d " " -f 1 - | grep -v USER | sort -u  #w - showed who is loggin on my machine
 echo -e "This is `uname -s` running on `uname -n` processor. \n"
-echo -e "This is the uptime information for you: \n"
+echo -e "This is the uptime information for you: \n" 
 
 #date - shows the date
 #date +"%V" - number of the week
